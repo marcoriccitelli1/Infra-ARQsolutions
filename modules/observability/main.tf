0eaf8afa3c4788ac7d58aee1780db0386ca4a1ac
jpsoron <55992235+jpsoron@users.noreply.github.com>
2025-09-21 20:52:03 +0000

    chore(fmt): apply terraform fmt via CI

--- modules/observability/main.tf
+++ modules/observability/main.tf
@@ -7,22 +7,22 @@ resource "aws_s3_bucket_policy" "cloudtrail_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Sid       = "AWSCloudTrailAclCheck",
-        Effect    = "Allow",
+        Sid    = "AWSCloudTrailAclCheck",
+        Effect = "Allow",
         Principal = {
           Service = "cloudtrail.amazonaws.com"
         },
-        Action    = "s3:GetBucketAcl",
-        Resource  = "arn:aws:s3:::${aws_s3_bucket.logs_bucket.id}"
+        Action   = "s3:GetBucketAcl",
+        Resource = "arn:aws:s3:::${aws_s3_bucket.logs_bucket.id}"
       },
       {
-        Sid       = "AWSCloudTrailWrite",
-        Effect    = "Allow",
+        Sid    = "AWSCloudTrailWrite",
+        Effect = "Allow",
         Principal = {
           Service = "cloudtrail.amazonaws.com"
         },
-        Action    = "s3:PutObject",
-        Resource  = "arn:aws:s3:::${aws_s3_bucket.logs_bucket.id}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+        Action   = "s3:PutObject",
+        Resource = "arn:aws:s3:::${aws_s3_bucket.logs_bucket.id}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
         Condition = {
           StringEquals = {
             "s3:x-amz-acl" = "bucket-owner-full-control"
